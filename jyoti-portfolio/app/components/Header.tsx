844d25dc1a7a5b6adb3289382b0a354a9ead2ce6
Jyoti K C <jyotikc017@gmail.com>
2024-09-08 10:45:13 +1000

    Handle special characters

--- jyoti-portfolio/app/components/Header.tsx
+++ jyoti-portfolio/app/components/Header.tsx
@@ -3,7 +3,6 @@ import NavLinks from "./NavLinks";
 import SocialMedia from "../components/SocialMedia";
 import Image from 'next/image';
 import Link from 'next/link';
-import j from '../../public/j.svg'
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Header() {
@@ -28,7 +27,7 @@ export default function Header() {
                         <Image src="https://i.pravatar.cc/500" height={500} width={500} alt="Profile" className="rounded" />
                     </div>
                     <div className="sm:w-2/3 sm:ml-4">
-                        <h1 className="text-slate-900 dark:text-white mt-5 text-3xl font-bold tracking-tight text-center sm:text-right">Hello, I'm Jyoti!</h1>
+                        <h1 className="text-slate-900 dark:text-white mt-5 text-3xl font-bold tracking-tight text-center sm:text-right">Hello, I&apos;m Jyoti!</h1>
                         <p className="text-slate-500 dark:text-slate-400 mt-4 text-xl font-semibold text-justify">A full stack developer with over a decade of experience in designing solutions that drive innovation for industry and society</p>
                         <div className="relative min-w-full border-t-2 mt-4 border-slate-400 before:content-[''] before:absolute before:left-0 before:-top-1.5 before:min-w-2 before:min-h-2 before:rounded-full before:bg-slate-400"></div>
                         <div className="w-full h-auto py-8 flex items-center justify-center sm:justify-end gap-4 flex-wrap">
