312a8c05e2c77e02722c53bd425c42dbac11a79e
Jyoti K C <jyotikc017@gmail.com>
2025-01-29 11:37:43 +1000

    Update Nextjs Config as per Nextjs15 for Image

--- jyoti-portfolio/next.config.mjs
+++ jyoti-portfolio/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        //domains: ['i.pravatar.cc'],
         remotePatterns: [
             {
                 protocol: 'https',
