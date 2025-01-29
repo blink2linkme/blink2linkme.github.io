5df89a4f8af0cbbe2840c1d39667c506687318a3
Jyoti K C <jyotikc017@gmail.com>
2025-01-29 11:46:55 +1000

    Update Footer block

--- jyoti-portfolio/app/components/Footer.tsx
+++ jyoti-portfolio/app/components/Footer.tsx
@@ -1,16 +1,17 @@
 import SocialMedia from "../components/SocialMedia";
 
+const currentYear = new Date(Date.now()).getFullYear();
 export default function Footer() {
     return (
-        <footer className="container mx-auto">
-            <p className="text-center py-10 text-slate-900 dark:text-white">
-                Let&apos;s connect -- <a href="mailto:hello@jyoti.name.np">hello@jyoti.name.np</a>
+        <footer className="container mx-auto pt-5 inset-x-0 bottom-0">
+            <p className="text-center py-0 text-slate-900 dark:text-white">
+                Let&apos;s connect -- <a href="mailto:hello@jyoti.name.np">jyotikc017@gmail.com</a>
             </p>
-            <div className="w-full h-auto py-8 flex items-center justify-center gap-4 flex-wrap">
+            <div className="w-full h-auto py-4 flex items-center justify-center gap-4 flex-wrap">
                 <SocialMedia />
             </div>
-            <span className="text-lg text-center block py-10 text-slate-900 dark:text-white">
-                &copy; <a href="https://jyoti.name.np/">Jyoti K C</a> 2024, All rights reserved.
+            <span className="text-center block py-4 text-slate-900 dark:text-white">
+                &copy; <a href="https://jyoti.name.np/">Jyoti K C</a> { currentYear }, All rights reserved.
             </span>
         </footer>
     );
