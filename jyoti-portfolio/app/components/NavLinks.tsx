5296e072b9fe60a8ad49218f9f9020e45833127f
Jyoti K C <jyotikc017@gmail.com>
2025-01-29 11:53:49 +1000

    Update NavLinks

--- jyoti-portfolio/app/components/NavLinks.tsx
+++ jyoti-portfolio/app/components/NavLinks.tsx
@@ -1,24 +1,17 @@
 import Link from "next/link";
-import menu from "../config/menu.json";
+import menus from "../config/menu.json";
 import { usePathname } from "next/navigation";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function NavLinks() {
     const pathName = usePathname();
     return (
-        <nav className="p-6">
-            <ul className="flex justify-center sm:justify-end">
-                {
-                    menu.map((m, i) =>
-                        <li key={i} className={`${
-                            i > 0 ? 'border-l-2 border-gray-300 dark:border-slate-600' : ''
-                          } ${
-                            pathName === m.path ? 'active-link' : ''
-                          }`}>
-                            <Link href={m.path} className="link dark:text:slate-200 text:gray-700 text-lg active-link:font-black px-3 transition-all duration-500 hover:-translate-y-2 line-height-10 hover:font-bold hover:scale-150 text-underline">{m.name}</Link>
-                        </li>
-                    )
-                }
-            </ul>
+        <nav className="flex items-center space-x-6">
+           {
+            menus.map((menu, index) => <Link key={index} href={menu.path ?? ''} className={`hover:text-blue-500 dark:hover:text-blue-400${pathName == menu.path ? ' active:bg-violet-700 underline': ''}`}>{menu.name}</Link>)
+           }
+            <ThemeSwitcher />
         </nav>
+       
     );
 }
